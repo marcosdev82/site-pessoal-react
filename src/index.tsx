295587c5aae978b162889ba3352d0f69be8f0729
mikek182 <marcosvinicius@geekwork.com.br>
2024-10-11 17:29:05 -0300

    Criando estilo global

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
 
-import Home from './pages/home';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +10,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,7 +1,21 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-    width: 100%;
-    height: 100vh;
-    background-color: black;
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html, border-style, #root{
+        height: 100%;
+    }
+    *, button, input {
+        border: 0;
+        outline: 0;
+        font-family: 'Roboto', sans-serif;
+    }
+    button {
+        cursor: pointer;
+    }
 `;
