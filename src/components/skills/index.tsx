2804260fa0adf5d9c84e5d4f948a33e1fcb309ac
mikek182 <marcosvinicius@geekwork.com.br>
2024-10-18 22:49:12 -0300

    teste de deploy

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -28,7 +28,7 @@ const Skills: React.FC = () => {
                     <li><IconSkill src={laravel} alt="Marcos Tavares Desenvolvedor Fulstack" /></li>
                     {/* <li><IconSkill src={php} alt="Marcos Tavares Desenvolvedor Fulstack" /></li> */}
                     {/* <li><IconSkill src={wordpress} alt="Marcos Tavares Desenvolvedor Fulstack" /></li> */}
-                    <li><IconSkill src={postgres} alt="Marcos Tavares Desenvolvedor Fulstack" /></li>
+                    {/* <li><IconSkill src={postgres} alt="Marcos Tavares Desenvolvedor Fulstack" /></li> */}
                     <li><IconSkill src={docker} alt="Marcos Tavares Desenvolvedor Fulstack" /></li>
                     <li><IconSkill src={redis} alt="Marcos Tavares Desenvolvedor Fulstack" /></li>
                 </ul>
