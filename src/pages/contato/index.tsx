e08998d98a4b45fba961412b786e48943e29eacc
mikek182 <marcosvinicius@geekwork.com.br>
2025-02-03 17:29:36 -0300

    Metags contato

--- src/pages/contato/index.tsx
+++ src/pages/contato/index.tsx
@@ -7,7 +7,20 @@ const Contato = () => {
         <>
             <Helmet>
                 <title>Contato - Marcos Tavares Desenvolvedor Fullstack</title>
-                <meta name="description" content="Contato" />
+                <meta name="description" content="Marcos Tavares, desenvolvedor fullstack especializado em Java Script, Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados." />
+                <link rel="canonical" href="https://marcostavares.dev.br/servicos" />
+                <meta name="keywords" content="desenvolvedor fullstack, desenvolvimento web, Contato, aplicações escaláveis, criação de sites, sistemas web, design responsivo, programação front-end, programação back-end, React, Node.js, Laravel, desenvolvimento de software, soluções digitais, programador fullstack, Marcos Tavares" />
+                <meta property="og:locale" content="pt_BR"/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:title" content="Contato - Marcos Tavares Fullstack"/>
+                <meta property="og:description" content="Marcos Tavares,  Contato, desenvolvedor fullstack especializado em Java Script, Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados."/>
+                <meta property="og:url" content="https://marcostavares.dev"/>
+                <meta property="og:site_name" content="Marcos Tavares FullStack"/>
+                <meta property="article:publisher" content="https://www.facebook.com/marcostavares.dev"/>
+                {/* <meta property="article:modified_time" content="2025-01-27T18:36:24+00:00"/> */}
+                {/* <meta name="twitter:card" content="summary_large_image"/>
+                <meta name="twitter:site" content="@marcostavares.dev"/> */}
+                {/* <meta name="google-site-verification" content=""/> */}
             </Helmet>
             <EntryTitle>
                 <h1>Contato</h1>
