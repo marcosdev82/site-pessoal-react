31bf97ca2edfa5a17b55afde02572d1ac3281b6c
mikek182 <marcosvinicius@geekwork.com.br>
2025-02-20 14:03:55 -0300

    formik formulário de contato teste

--- src/pages/contato/index.tsx
+++ src/pages/contato/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Helmet } from 'react-helmet-async';
 import { EntryTitle } from './styles';
 
@@ -25,6 +26,41 @@ const Contato = () => {
             </Helmet>
             <EntryTitle>
                 <h1>Contato</h1>
+                <div>
+                    <h1>Any place in your app!</h1>
+                    <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validate={values => {
+                        const errors = {};
+                        if (!values.email) {
+                        errors.email = 'Required';
+                        } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                        ) {
+                        errors.email = 'Invalid email address';
+                        }
+                        return errors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                        }, 400);
+                    }}
+                    >
+                    {({ isSubmitting }) => (
+                        <Form>
+                        <Field type="email" name="email" />
+                        <ErrorMessage name="email" component="div" />
+                        <Field type="password" name="password" />
+                        <ErrorMessage name="password" component="div" />
+                        <button type="submit" disabled={isSubmitting}>
+                            Submit
+                        </button>
+                        </Form>
+                    )}
+                    </Formik>
+                </div>
             </EntryTitle>
         </>
     );
