0b7c6db48042bd34e695367014e8d5af54d72063
mikek182 <marcosvinicius@geekwork.com.br>
2025-02-20 14:19:39 -0300

    Refatoração da home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,34 +9,30 @@ const Home = () => {
         <>
             <Helmet>
                 <title>Home - Marcos Tavares Desenvolvedor Fullstack</title>
-                <meta name="description" content="Marcos Tavares, desenvolvedor fullstack especializado em Java Script, Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados." />
+                <meta name="description" content="Marcos Tavares, desenvolvedor fullstack especializado em JavaScript. Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados." />
                 <link rel="canonical" href="https://marcostavares.dev.br/" />
+                <meta name="robots" content="index, follow" />
                 <meta name="keywords" content="desenvolvedor fullstack, desenvolvimento web, aplicações escaláveis, criação de sites, sistemas web, design responsivo, programação front-end, programação back-end, React, Node.js, Laravel, desenvolvimento de software, soluções digitais, programador fullstack, Marcos Tavares" />
-                <meta property="og:locale" content="pt_BR"/>
-                <meta property="og:type" content="website"/>
-                <meta property="og:title" content="home - Marcos Tavares Fullstack"/>
-                <meta property="og:description" content="Marcos Tavares, desenvolvedor fullstack especializado em Java Script, Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados."/>
-                <meta property="og:url" content="https://marcostavares.dev"/>
-                <meta property="og:site_name" content="Marcos Tavares FullStack"/>
-                <meta property="article:publisher" content="https://www.facebook.com/marcostavares.dev"/>
-                {/* <meta property="article:modified_time" content="2025-01-27T18:36:24+00:00"/> */}
-                {/* <meta name="twitter:card" content="summary_large_image"/>
-                <meta name="twitter:site" content="@marcostavares.dev"/> */}
-                {/* <meta name="google-site-verification" content=""/> */}
+                <meta property="og:locale" content="pt_BR" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Home - Marcos Tavares Fullstack" />
+                <meta property="og:description" content="Marcos Tavares, desenvolvedor fullstack especializado em JavaScript. Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados." />
+                <meta property="og:url" content="https://marcostavares.dev.br/" />
+                <meta property="og:site_name" content="Marcos Tavares FullStack" />
+                <meta property="article:publisher" content="https://www.facebook.com/marcostavares.dev" />
             </Helmet>
             
             <About>
                 <Description>
-                    {/* <h1>MARCOS TAVARES</h1> */}
                     <h2>DESENVOLVEDOR FULLSTACK</h2>
-                    <p>Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados..</p>
+                    <p>Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados.</p>
                     <SocialMedia>
                         <Curriculo>
-                            <a href='http://marcostavares.dev.br/'><span>Download CV</span> <FileDownload style={{ fontSize: 16 }}/></a>
+                            <a href="http://marcostavares.dev.br/" download><span>Download CV</span> <FileDownload style={{ fontSize: 16 }} /></a>
                         </Curriculo>
-                        <a href='https://www.linkedin.com/in/marcostavares-dev/' target="_blank" className="social" rel="noreferrer"><LinkedIn style={{ fontSize: 18 }} /></a>
-                        <a href='https://www.instagram.com/marcostavares.dev/' target="_blank" className="social" rel="noreferrer"><Instagram style={{ fontSize: 18 }}/></a>
-                        <a href='https://github.com/marcosdev82' target="_blank"  className="social" rel="noreferrer"><GitHub style={{ fontSize: 16 }}/></a>
+                        <a href="https://www.linkedin.com/in/marcostavares-dev/" target="_blank" className="social" rel="noopener noreferrer"><LinkedIn style={{ fontSize: 18 }} /></a>
+                        <a href="https://www.instagram.com/marcostavares.dev/" target="_blank" className="social" rel="noopener noreferrer"><Instagram style={{ fontSize: 18 }} /></a>
+                        <a href="https://github.com/marcosdev82" target="_blank" className="social" rel="noopener noreferrer"><GitHub style={{ fontSize: 16 }} /></a>
                     </SocialMedia>
                 </Description>
                 <Portrait>
