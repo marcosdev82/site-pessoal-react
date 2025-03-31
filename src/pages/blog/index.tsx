b27ebc939ec91bbc200000dfc521922cea43ca7e
mikek182 <marcosvinicius@geekwork.com.br>
2025-03-31 11:39:41 -0300

    Estrutura do bloq

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -14,13 +14,13 @@ const Blog = () => {
                 <link rel="canonical" href="https://marcostavares.dev.br/blog" />
                 <meta name="robots" content="index, follow" />
                 <meta name="keywords" content="desenvolvedor fullstack, desenvolvimento web, aplicações escaláveis, criação de sites, sistemas web, design responsivo, programação front-end, programação back-end, React, Node.js, Laravel, desenvolvimento de software, soluções digitais, programador fullstack, Marcos Tavares" />
-                <meta property="og:locale" content="pt_BR"/> 
-                <meta property="og:type" content="website"/>
-                <meta property="og:title" content="Blog - Marcos Tavares Fullstack"/>
-                <meta property="og:description" content="Marcos Tavares, desenvolvedor fullstack especializado em JavaScript. Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados."/>
-                <meta property="og:url" content="https://marcostavares.dev.br/blog"/>
-                <meta property="og:site_name" content="Marcos Tavares FullStack"/>
-                <meta property="article:publisher" content="https://www.facebook.com/marcostavares.dev"/>
+                <meta property="og:locale" content="pt_BR" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Blog - Marcos Tavares Fullstack" />
+                <meta property="og:description" content="Marcos Tavares, desenvolvedor fullstack especializado em JavaScript. Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados." />
+                <meta property="og:url" content="https://marcostavares.dev.br/blog" />
+                <meta property="og:site_name" content="Marcos Tavares FullStack" />
+                <meta property="article:publisher" content="https://www.facebook.com/marcostavares.dev" />
             </Helmet>
 
             <EntryTitle title='Blog' />
@@ -29,7 +29,7 @@ const Blog = () => {
                 <Sidebar />
                 <ListPosts />
             </EntryContent>
-    
+
         </>
     );
 };
