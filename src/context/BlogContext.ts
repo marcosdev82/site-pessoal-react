98d9632f095a46b8df1647d60538bcf864f9e8ac
mikek182 <marcosvinicius@geekwork.com.br>
2025-03-31 13:05:24 -0300

    Criação do provide para API

---
+++ src/context/BlogContext.ts
@@ -0,0 +1,41 @@
+// src/context/BlogContext.tsx
+import { createContext, useState, useEffect } from "react";
+import { getPosts, getCategories } from "../service/api";
+
+// Tipos para os dados
+interface Post  {
+  id: number;
+  title: string;
+  content: string;
+  categoryId: number;
+};
+
+interface Category {
+  id: number;
+  name: string;
+};
+
+// Tipagem do contexto
+interface BlogContextType {
+  posts: Post[];
+  categories: Category[];
+};
+
+// Criando o contexto com valor inicial `null`
+export const BlogContext = createContext<BlogContextType | null>(null);
+
+export const BlogProvider = ({ children }) => {
+  const [posts, setPosts] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getPosts().then(setPosts);
+    getCategories().then(setCategories);
+  }, []);
+
+  return (
+    <BlogContext.Provider value={{ posts, categories }}>
+      {children}
+    </BlogContext.Provider>
+  );
+};
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,37 +1,9 @@
 import React from 'react';
-import { Helmet } from 'react-helmet-async';
-import EntryTitle from '../../components/entrytitle';
-import Sidebar from '../../components/sidebar';
-import ListPosts from './listPosts';
-import { EntryContent } from './styles';
+import { BlogProvider } from '../../context/BlogContext';
 
 const Blog = () => {
-    return (
-        <>
-            <Helmet>
-                <title>Blog - Marcos Tavares Desenvolvedor Fullstack</title>
-                <meta name="description" content="Marcos Tavares, desenvolvedor fullstack especializado em JavaScript. Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados." />
-                <link rel="canonical" href="https://marcostavares.dev.br/blog" />
-                <meta name="robots" content="index, follow" />
-                <meta name="keywords" content="desenvolvedor fullstack, desenvolvimento web, aplicações escaláveis, criação de sites, sistemas web, design responsivo, programação front-end, programação back-end, React, Node.js, Laravel, desenvolvimento de software, soluções digitais, programador fullstack, Marcos Tavares" />
-                <meta property="og:locale" content="pt_BR" />
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content="Blog - Marcos Tavares Fullstack" />
-                <meta property="og:description" content="Marcos Tavares, desenvolvedor fullstack especializado em JavaScript. Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados." />
-                <meta property="og:url" content="https://marcostavares.dev.br/blog" />
-                <meta property="og:site_name" content="Marcos Tavares FullStack" />
-                <meta property="article:publisher" content="https://www.facebook.com/marcostavares.dev" />
-            </Helmet>
-
-            <EntryTitle title='Blog' />
-
-            <EntryContent>
-                <Sidebar />
-                <ListPosts />
-            </EntryContent>
-
-        </>
-    );
-};
+    return ();
+    )
+}
 
 export default Blog;
