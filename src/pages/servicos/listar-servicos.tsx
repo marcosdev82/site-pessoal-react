3096e6157d945386a8c1a224fb70f6c935b06ad8
mikek182 <marcosvinicius@geekwork.com.br>
2025-07-09 15:21:48 -0300

    Icones forn awesome

--- src/pages/servicos/listar-servicos.tsx
+++ src/pages/servicos/listar-servicos.tsx
@@ -1,20 +1,62 @@
 import React from "react";
-// import { Button, Card, ListGroup } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faLaptopCode,
+    faCogs,
+    faMobileAlt,
+    faPlug,
+    faTachometerAlt,
+    faCloud
+} from '@fortawesome/free-solid-svg-icons';
+import { faWordpress } from '@fortawesome/free-brands-svg-icons';
 
 function ListarSevicos() {
-
     return (
-        <ul>
-            <li><div>Desenvolvimento de sites institucionais e landing pages</div></li>
-            <li><div>Criação de sistemas personalizados com React, Node.js e Laravel</div></li>
-            <li><div>Criação de Aplicativos com React</div></li>
-            <li><div>Integrações com APIs e automações</div></li>
-            <li><div>Otimização de performance e SEO técnico</div></li>
-            <li><div>Criação e manutenção de temas e plugins WordPress</div></li>
-            <li><div>Hospedagem de sites e sistemas</div></li>
+        <ul className="space-y-2">
+            <li>
+                <div>
+                    <FontAwesomeIcon icon={faLaptopCode} className="text-blue-600 mr-2" />
+                    Desenvolvimento de sites institucionais e landing pages
+                </div>
+            </li>
+            <li>
+                <div>
+                    <FontAwesomeIcon icon={faCogs} className="text-green-600 mr-2" />
+                    Criação de sistemas personalizados com React, Node.js e Laravel
+                </div>
+            </li>
+            <li>
+                <div>
+                    <FontAwesomeIcon icon={faMobileAlt} className="text-indigo-600 mr-2" />
+                    Criação de Aplicativos com React
+                </div>
+            </li>
+            <li>
+                <div>
+                    <FontAwesomeIcon icon={faPlug} className="text-yellow-600 mr-2" />
+                    Integrações com APIs e automações
+                </div>
+            </li>
+            <li>
+                <div>
+                    <FontAwesomeIcon icon={faTachometerAlt} className="text-red-600 mr-2" />
+                    Otimização de performance e SEO técnico
+                </div>
+            </li>
+            <li>
+                <div>
+                    <FontAwesomeIcon icon={faWordpress} className="text-purple-600 mr-2" />
+                    Criação e manutenção de temas e plugins WordPress
+                </div>
+            </li>
+            <li>
+                <div>
+                    <FontAwesomeIcon icon={faCloud} className="text-gray-600 mr-2" />
+                    Hospedagem de sites e sistemas
+                </div>
+            </li>
         </ul>
     );
-
 }
 
 export default ListarSevicos;
