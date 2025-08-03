3fb11982e44fe3f4f172d38f7ee2f5e5e210fc96
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-03 13:07:10 -0300

    Correção das metas

--- src/pages/sobre/index.tsx
+++ src/pages/sobre/index.tsx
@@ -13,18 +13,19 @@ const Sobre = () => {
                 </p>
             </Content>
 
-            <title>Sobre min - Marcos Tavares Desenvolvedor full stack</title>
-            <meta name="description" content="Marcos Tavares, desenvolvedor full stack especializado em JavaScript. Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados." />
+            <title>Sobre mim - Marcos Tavares Desenvolvedor Full Stack</title>
+            <meta name="description" content="Marcos Tavares, desenvolvedor full stack natural de Porto Alegre (RS), atualmente morando em Fortaleza (CE). Formado em Análise e Desenvolvimento de Sistemas, apaixonado por desenvolvimento de software e aprendizado contínuo. Crio soluções digitais robustas, escaláveis e intuitivas, unindo design e funcionalidade para gerar resultados." />
             <link rel="canonical" href="https://marcostavares.dev.br/sobre" />
             <meta name="robots" content="index, follow" />
-            <meta name="keywords" content="desenvolvedor full stack, desenvolvimento web, aplicações escaláveis, criação de sites, sistemas web, design responsivo, programação front-end, programação back-end, React, Node.js, Laravel, desenvolvimento de software, soluções digitais, programador full stack, Marcos Tavares" />
+            <meta name="keywords" content="desenvolvedor full stack, desenvolvimento web, aplicações escaláveis, criação de sites, sistemas web, design responsivo, programação front-end, programação back-end, React, Node.js, Laravel, desenvolvimento de software, soluções digitais, programador full stack, Marcos Tavares, Fortaleza, Porto Alegre" />
             <meta property="og:locale" content="pt_BR" />
             <meta property="og:type" content="website" />
-            <meta property="og:title" content="Sobre - Marcos Tavares full stack" />
-            <meta property="og:description" content="Marcos Tavares, desenvolvedor full stack especializado em JavaScript. Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados." />
+            <meta property="og:title" content="Sobre mim - Marcos Tavares Desenvolvedor Full Stack" />
+            <meta property="og:description" content="Marcos Tavares, desenvolvedor full stack natural de Porto Alegre (RS), atualmente em Fortaleza (CE). Formado em Análise e Desenvolvimento de Sistemas e apaixonado por tecnologia, cria soluções digitais robustas, escaláveis e funcionais." />
             <meta property="og:url" content="https://marcostavares.dev.br/sobre" />
-            <meta property="og:site_name" content="Marcos Tavares full stack" />
+            <meta property="og:site_name" content="Marcos Tavares Full Stack" />
             <meta property="article:publisher" content="https://www.facebook.com/marcostavares.dev" />
+
         </>
     );
 };
