d65b18d49e58019c8ee22ac23670925dee408160
mikek182 <marcosvinicius@geekwork.com.br>
2025-08-20 15:34:17 -0300

    Metas dinâmicas

--- src/pages/blog/single/PostContent.tsx
+++ src/pages/blog/single/PostContent.tsx
@@ -70,25 +70,25 @@ const SinglePost = () => {
                 </Content>
             </EntryContent>
 
-            <title>Blog - TÍTULO_DO_POST</title>
+            <title>{`Blog - ${title}`}</title>
 
             <meta name="description" content="Marcos Tavares, desenvolvedor full stack especializado em JavaScript. 
                 Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas que unem design e funcionalidade para entregar excelência e resultados." />
             <meta name="keywords" content="desenvolvedor full stack, desenvolvimento web, aplicações escaláveis, criação de sites, 
                 sistemas web, design responsivo, programação front-end, programação back-end, React, Node.js, Laravel, desenvolvimento de software, soluções digitais, programador full stack, Marcos Tavares" />
             <meta name="robots" content="index, follow" />
-            <link rel="canonical" href="https://marcostavares.dev.br/blog/SLUG_DO_POST" />
+            <link rel="canonical" href={`https://marcostavares.dev.br/blog/${title}`} />
             <meta property="og:type" content="website" />
-            <meta property="og:title" content="Blog - TÍTULO_DO_POST" />
+            <meta property="og:title" content={`Blog - ${title}`} />
             <meta property="og:description" content="Marcos Tavares, desenvolvedor full stack especializado em JavaScript. 
                 Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas 
                 que unem design e funcionalidade para entregar excelência e resultados." />
             <meta property="og:site_name" content="Marcos Tavares full stack" />
-            <meta property="og:url" content="https://marcostavares.dev.br/blog/SLUG_DO_POST" />
+            <meta property="og:url" content={`https://marcostavares.dev.br/blog/${title}`} />
             <meta property="og:locale" content="pt_BR" />
             <meta property="article:author" content="https://www.facebook.com/marcostavares.dev" />
             <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:title" content="Blog - TÍTULO_DO_POST" />
+            <meta name="twitter:title" content={`Blog - ${title}`} />
             <meta name="twitter:description" content="Marcos Tavares, desenvolvedor full stack especializado em JavaScript. 
                 Dê vida às suas ideias com soluções digitais inteligentes. Desenvolvo aplicações robustas, escaláveis e intuitivas 
                 que unem design e funcionalidade para entregar excelência e resultados." />
