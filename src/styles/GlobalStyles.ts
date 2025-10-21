ecd7e54e565e5befd22ed754b9bae781c9f56df5
marcosdev82 <marcostavares.dev@hotmail.com>
2025-10-21 08:34:12 -0300

    background svg

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+const portraitImg  = '../../assets/images/background-node.svg';
+
 export default createGlobalStyle`
     * {
         margin: 0;
@@ -9,11 +11,14 @@ export default createGlobalStyle`
     html {
         scrollbar-width: thin; /* Largura fina da scrollbar */
         scrollbar-color: #072326 ${props => props.theme.colors.tertiary}; /* Polegar (thumb) e fundo (track) */
-    }
+    } 
     html, body, #root{
         height: 100vh;
         background-color:#070c15;
         flex: 1;
+    }
+    body {
+        background-image: url(${portraitImg});
     }
     *, button, input {
         border: 0;
