3e4b993f7eadc83b09d1fdfbfaba84774570abb7
marcosdev82 <marcostavares.dev@hotmail.com>
2025-11-09 15:44:20 -0300

    Fazendo com que o react ignore rotas do wordpress

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import App from './App';
+const pathname = window.location.pathname;
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-
-root.render(
+// Se a rota começar com /wp-admin ou /wp-login, não renderiza o React
+if (
+  pathname.startsWith("/wp-admin") ||
+  pathname.startsWith("/wp-login") ||
+  pathname.startsWith("/wpmt/wp-admin") ||
+  pathname.startsWith("/wpmt/wp-login")
+) {
+  console.warn("Rota administrativa detectada — React ignorado.");
+} else {
+  ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+      <App />
     </React.StrictMode>
-);
+  );
+}
